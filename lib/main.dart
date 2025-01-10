6f7ccc047738f38e1c54fcafc71cf1afebad2ddd
Saksham gupta <guptasaksham3071gmail.com>
2025-01-10 12:41:10 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:blog_app/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Blog App',
+      theme: ThemeData(
+        fontFamily: 'Sora',
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color(0xff09254A),
+          primary: const Color(0xff09254A),
+        ),
+        useMaterial3: false,
+      ),
+    );
+  }
+}
